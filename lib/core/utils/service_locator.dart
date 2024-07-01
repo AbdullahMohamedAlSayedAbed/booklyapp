44d73fefaadb5e0e108eecb6b41f386feb562945
Abdullah Abed <abedabdallah834@gmail.com>
2024-07-01 09:33:07 +0300

    add search service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:booklyapp/Features/home/data/repos/home_repo_implemtion.dart';
+import 'package:booklyapp/Features/search/data/repo/search_repo_impl.dart';
 import 'package:booklyapp/core/utils/api_service.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
@@ -13,5 +14,8 @@ void setupServiceLocator() {
     HomeRepoImpl(
       apiService: getIt.get<ApiService>(),
     ),
+  );
+  getIt.registerSingleton<SearchRepoImpl>(
+    SearchRepoImpl( getIt.get<ApiService>()),
   );
 }
